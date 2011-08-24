46c8ae2fac692e0979cbc5075f5d76e113e113de
Max Kamashev <max.kamashev@gmail.com>
2011-08-24 19:02:14 +0400

    Small changes

---
+++ application/classes/command/keys.php
@@ -0,0 +1,9 @@
+<?php
+
+class Command_Keys
+{
+    public function keys()
+    {
+
+    }
+}
--- application/view/tables/zrange.php
+++ application/view/tables/zrange.php
@@ -3,16 +3,20 @@
         <caption>ZRANGE <?= $key ?> 0 -1</caption>
         <thead>
             <tr>
-                <th>value</th>
+                <th class="column span-2">id</th>
+                <th class="column span-3">value</th>
                 <th>score</th>
             </tr>
         </thead>
         <tbody>
+            <?php $i = 1; ?>
             <?php foreach( $value as $k => $v ) : ?>
             <tr>
+                <td><?= $i ?></td>
                 <td><?= $k ?></td>
                 <td><?= $v ?></td>
             </tr>
+            <?php $i++; ?>
             <?php endforeach; ?>
         </tbody>
     </table>
