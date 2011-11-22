e50cbf7a11e81aea0e1b3e61b536fb57f10bfd1a
Max Kamashev <max.kamashev@gmail.com>
2011-11-22 16:23:53 +0400

    google analytics

--- application/view/head.php
+++ application/view/head.php
@@ -32,4 +32,18 @@
     <link href="/css/bootstrap.css" rel="stylesheet">
     <link href="/css/smoothness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
     <link href="/css/main.css" rel="stylesheet" />
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27209908-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
